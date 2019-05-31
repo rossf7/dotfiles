58b0dae553067ab638e65e0d54716c84f36b196f
Ross Fairbanks <ross@giantswarm.io>
2019-05-31 18:04:45 +0200

    Fix whitespace

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-
 # Create zsh symlinks
 ln -sf ~/.dotfiles/zsh/.aliases ~/.aliases
 ln -sf ~/.dotfiles/zsh/.zshenv ~/.zshenv
