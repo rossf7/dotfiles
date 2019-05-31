21425b76a74b3e03b914e2edfceec9d3bdc65fb4
Ross Fairbanks <ross@giantswarm.io>
2019-05-31 18:19:26 +0200

    add git

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 ln -sf ~/.dotfiles/zsh/.aliases ~/.aliases
 ln -sf ~/.dotfiles/zsh/.zshenv ~/.zshenv
 ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
+
+# Create git symlinks
+ln -sf ~/.dotfiles/git/.gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
